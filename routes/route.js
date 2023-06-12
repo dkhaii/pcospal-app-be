2d4b05d7c1b5048738190acfe4930a50905d961e
gerinmordekhai <mordekhaigerinlumangkun@gmail.com>
2023-06-12 17:29:41 +0700

    added easter egg

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const { createUser, loginUser } = require('../controllers/userController');
+const { responseClient } = require('../helper');
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+  res.status(200).json(
+    responseClient(
+      'success',
+      'Selamat Datang di Welcome :) xixi',
+      {
+        backendDev: '@gerinmordekhaii',
+        cloudEng: 'bang habib',
+      },
+    ),
+  );
+});
 router.post('/register', createUser);
 router.post('/login', loginUser);
 
