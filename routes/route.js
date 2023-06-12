f2edeee7ca7745106b29f4c413f478615360fe70
gerinmordekhai <mordekhaigerinlumangkun@gmail.com>
2023-06-12 17:49:02 +0700

    added easter egg

--- routes/route.js
+++ routes/route.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const { createUser, loginUser } = require('../controllers/userController');
-const { responseClient } = require('../helper');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  res.status(200).json(
-    responseClient(
-      'success',
-      'Selamat Datang di Welcome :) xixi',
-      {
-        backendDev: 'gerin tampan sekali',
-        cloudEng: 'bang habib sang penakluk awan google',
-      },
-    ),
-  );
-});
 router.post('/register', createUser);
 router.post('/login', loginUser);
 
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 
 const routes = require('./routes/route');
 const authRoutes = require('./routes/authRoutes');
+const { responseClient } = require('./helper');
 
 const app = express();
 const port = 8080;
+const router = express.Router();
 
 app.use(bodyParser.json());
 app.use('/api', routes);
@@ -13,4 +15,17 @@ app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`app is running in port ${port}`);
+});
+
+router.post('/', (req, res) => {
+  res.status(200).json(
+    responseClient(
+      'success',
+      'Selamat Datang di Welcome :) xixi',
+      {
+        backendDev: 'gerin tampan sekali',
+        cloudEng: 'bang habib sang penakluk awan google',
+      },
+    ),
+  );
 });
