cd3b910e4707e1648d7a4d265884b48987f53627
gerinmordekhai <mordekhaigerinlumangkun@gmail.com>
2023-06-28 14:25:21 +0700

    test cloudbuild v14

--- config/environment.js
+++ config/environment.js
@@ -1,35 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-const yaml = require('yaml');
+// const fs = require('fs');
+// const path = require('path');
+// const yaml = require('yaml');
 
-const parseAppYaml = () => {
-  const filePath = path.resolve('./app.yaml');
-  const appYaml = fs.readFileSync(filePath, 'utf8');
-  const appYamlData = yaml.parse(appYaml);
+// const parseAppYaml = () => {
+//   const filePath = path.resolve('./app.yaml');
+//   const appYaml = fs.readFileSync(filePath, 'utf8');
+//   const appYamlData = yaml.parse(appYaml);
 
-  const {
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_DIALECT,
-    DB_SOCKET,
-    APP_ENV,
-    JWT_SECRET_KEY,
-    OPENAI_API_KEY,
-  } = appYamlData.env_variables;
+//   const {
+//     DB_HOST,
+//     DB_USER,
+//     DB_PASSWORD,
+//     DB_DATABASE,
+//     DB_DIALECT,
+//     DB_SOCKET,
+//     APP_ENV,
+//     JWT_SECRET_KEY,
+//     OPENAI_API_KEY,
+//   } = appYamlData.env_variables;
 
-  return {
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_DIALECT,
-    DB_SOCKET,
-    APP_ENV,
-    JWT_SECRET_KEY,
-    OPENAI_API_KEY,
-  };
-};
+//   return {
+//     DB_HOST,
+//     DB_USER,
+//     DB_PASSWORD,
+//     DB_DATABASE,
+//     DB_DIALECT,
+//     DB_SOCKET,
+//     APP_ENV,
+//     JWT_SECRET_KEY,
+//     OPENAI_API_KEY,
+//   };
+// };
 
-module.exports = parseAppYaml;
+// module.exports = parseAppYaml;
